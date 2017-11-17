6c7720660b08e321dcb2c3f31b4518afbdbb9d21
asotodominguez <asotodominguez@victoria16.danielcastelao.org>
2017-11-17 09:10:14 +0100

    C

--- src/excod1av/ExCOD1Av.java
+++ src/excod1av/ExCOD1Av.java
@@ -19,6 +19,7 @@ public class ExCOD1Av {
         System.out.println("Commit B");
         System.out.println("Commit E");
         System.out.println("Commit F");
+        System.out.println("Commit C");
     }
     
 }
